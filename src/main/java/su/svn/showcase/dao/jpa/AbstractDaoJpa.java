e1edf7402dc28ef147d74c52357f99eca82488ce
Victor N. Skurikhin <vskurikhin@gmail.com>
2020-02-07 17:14:00 +0300

    Added two methods to Abstract DAO JPA class: first - saves a given entity. second - saves all given entities.

--- src/main/java/su/svn/showcase/dao/jpa/AbstractDaoJpa.java
+++ src/main/java/su/svn/showcase/dao/jpa/AbstractDaoJpa.java
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2020.02.07 12:46 by Victor N. Skurikhin.
+ * This file was last modified at 2020.02.07 17:14 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * AbstractDaoJpa.java
@@ -15,9 +15,11 @@ import su.svn.showcase.domain.DBEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 /**
  * Abstract Data-Access Object class to be implemented by all DAO's.
@@ -53,10 +55,10 @@ abstract class AbstractDaoJpa<K, E extends DBEntity<K>> implements Dao<K, E> {
     }
 
     /**
-     * Retrieves the all records of entity by namedQuery.
+     * Retrieves all records of entity by namedQuery.
      *
      * @param namedQuery - query.
-     * @return list of records of entity.
+     * @return records of entity by query.
      */
     protected List<E> abstractDaoFindAll(String namedQuery) {
         EntityManager em = getEntityManager();
@@ -72,9 +74,9 @@ abstract class AbstractDaoJpa<K, E extends DBEntity<K>> implements Dao<K, E> {
      * Retrieves the record of entity by key.
      *
      * @param id - key.
-     * @return record of entity.
+     * @return record of entity by key.
      */
-    Optional<E> abstractDaoFindById(K id) {
+    protected Optional<E> abstractDaoFindById(K id) {
         EntityManager em = getEntityManager();
         try {
             E entry = em.find(getEClass(), id);
@@ -83,6 +85,67 @@ abstract class AbstractDaoJpa<K, E extends DBEntity<K>> implements Dao<K, E> {
             getLogger().error("Can't search because had the exception", e);
             return Optional.empty();
         }
+    }
+
+    /**
+     * Saves a given entity.
+     * Use the true if further operations as the save operation might have changed
+     * the entity instance completely.
+     *
+     * @param entity must not be {@literal null}.
+     * @return {@literal true} if an entity saved, {@literal false} otherwise.
+     */
+    protected boolean abstractDaoSave(E entity) {
+        EntityManager em = getEntityManager();
+        try {
+            if (null == entity.getId()) {
+                em.persist(entity);
+                em.flush();
+            } else {
+                em.merge(entity);
+                em.flush();
+            }
+        } catch (RuntimeException e) {
+            getLogger().error("Can't save because had the exception", e);
+            return false;
+        }
+        getLogger().info("Save {} with id: {}", getEClass().getSimpleName(), entity.getId());
+        return true;
+    }
+
+    protected boolean abstractDaoSave(Supplier<E> supplier) {
+        return abstractDaoSave(supplier.get());
+    }
+
+    /**
+     * Saves all given entities.
+     *
+     * @param entities must not be {@literal null}.
+     * @return {@literal true} if all entities saved, {@literal false} otherwise.
+     */
+    protected boolean abstractDaoSaveAll(Iterable<? extends E> entities) {
+        List<K> ids = new ArrayList<>();
+        EntityManager em = getEntityManager();
+        try {
+            if (entities == null) {
+                return false;
+            }
+
+            for (E entity : entities) {
+                if (null == entity.getId()) {
+                    em.persist(entity);
+                } else {
+                    em.merge(entity);
+                }
+                ids.add(entity.getId());
+            }
+            em.flush();
+        } catch (RuntimeException e) {
+            getLogger().error("Can't save because had the exception", e);
+            return false;
+        }
+        getLogger().info("Save {} with ids: {}", getEClass().getSimpleName(), ids);
+        return true;
     }
 
     protected void close() {
