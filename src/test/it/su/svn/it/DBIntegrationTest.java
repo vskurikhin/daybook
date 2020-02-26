d8aebed3cf1c0cd35c8cb566cf211ded1983b75b
Victor N. Skurikhin <vskurikhin@gmail.com>
2020-02-26 18:29:21 +0300

    Added TagCloudView.

--- src/test/it/su/svn/it/DBIntegrationTest.java
+++ src/test/it/su/svn/it/DBIntegrationTest.java
@@ -216,11 +216,6 @@ public class DBIntegrationTest extends BaseIntegrationTest {
     public void test_recordFullCrudService_create() throws Exception {
         RecordFullDto dto = cloneRecordFullDto(2);
         dto.setTags(Collections.emptySet());
-//        if ( dto.getNewsEntry() instanceof NewsEntryFullDto) {
-//            NewsEntryFullDto newsEntryFullDto = (NewsEntryFullDto) dto.getNewsEntry();
-//            newsEntryFullDto.setRecord(null);
-//        }
-//        dto.setUserLogin(null);
         recordFullCrudService.create(dto);
     }
 
