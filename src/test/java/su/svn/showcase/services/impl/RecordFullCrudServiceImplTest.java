d4c7adb78b777b849c518b77484b75167a5f7ac6
Victor N. Skurikhin <vskurikhin@gmail.com>
2020-03-01 17:23:32 +0300

    fix #74 issue in RecordFullCrudServiceImplTest.

--- src/test/java/su/svn/showcase/services/impl/RecordFullCrudServiceImplTest.java
+++ src/test/java/su/svn/showcase/services/impl/RecordFullCrudServiceImplTest.java
@@ -14,14 +14,14 @@ import org.jboss.weld.junit5.WeldSetup;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import su.svn.showcase.dao.NewsEntryDao;
+import su.svn.showcase.dao.RecordDao;
 import su.svn.showcase.dao.UserLoginDao;
-import su.svn.showcase.dao.jpa.NewsEntryDaoJpa;
-import su.svn.showcase.domain.NewsEntry;
+import su.svn.showcase.dao.jpa.RecordDaoJpa;
+import su.svn.showcase.domain.Record;
 import su.svn.showcase.domain.UserLogin;
-import su.svn.showcase.dto.NewsEntryFullDto;
+import su.svn.showcase.dto.RecordFullDto;
 import su.svn.showcase.services.CrudService;
-import su.svn.showcase.services.NewsEntryFullCrudService;
+import su.svn.showcase.services.RecordFullCrudService;
 import su.svn.showcase.services.impl.support.EntityManagerFactoryProducer;
 import su.svn.showcase.services.impl.support.EntityManagerProducer;
 import su.svn.showcase.services.impl.support.JtaEnvironment;
@@ -43,14 +43,14 @@ import java.util.function.Function;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static su.svn.showcase.domain.TestData.cloneNewsEntry1;
+import static su.svn.showcase.domain.TestData.cloneRecord1;
 import static su.svn.showcase.domain.TestData.cloneUserLogin1;
-import static su.svn.showcase.dto.TestData.cloneNewsEntryFullDto1;
+import static su.svn.showcase.dto.TestData.cloneRecordFullDto1;
 import static su.svn.showcase.services.impl.support.EntityManagerFactoryProducer.configure;
 
 @SuppressWarnings("Convert2Diamond")
-@DisplayName("A NewsEntryFullCrudServiceImplTest unit test cases")
-@AddPackages(value = {NewsEntryDao.class, CrudService.class})
+@DisplayName("A RecordFullCrudServiceImplTest unit test cases")
+@AddPackages(value = {RecordDao.class, CrudService.class})
 @ExtendWith({JtaEnvironment.class, WeldJunit5Extension.class})
 class RecordFullCrudServiceImplTest {
 
@@ -62,8 +62,8 @@ class RecordFullCrudServiceImplTest {
     @WeldSetup
     private
     WeldInitiator weld = WeldInitiator.from(
-            NewsEntryDaoJpa.class,
-            NewsEntryFullCrudServiceImpl.class,
+            RecordDaoJpa.class,
+            RecordFullCrudServiceImpl.class,
             EntityManagerFactoryProducer.class,
             EntityManagerProducer.class)
             .activate(RequestScoped.class)
@@ -73,14 +73,14 @@ class RecordFullCrudServiceImplTest {
             .inject(this)
             .build();
 
-    private NewsEntryDao mockDao = mock(NewsEntryDao.class);
+    private RecordDao mockDao = mock(RecordDao.class);
     private UserLoginDao mockUserLoginDao = mock(UserLoginDao.class);
-    private NewsEntryFullCrudService mockService = mock(NewsEntryFullCrudService.class);
+    private RecordFullCrudService mockService = mock(RecordFullCrudService.class);
 
     private Map<String, Object> ejbMap = new HashMap<String, Object>() {{
         put(null,                                     mockDao);
-        put(NewsEntryDao.class.getName(),             mockDao);
-        put(NewsEntryFullCrudService.class.getName(), mockService);
+        put(RecordDao.class.getName(),             mockDao);
+        put(RecordFullCrudService.class.getName(), mockService);
         put(UserLoginDao.class.getName(),             mockUserLoginDao);
     }};
 
@@ -94,14 +94,14 @@ class RecordFullCrudServiceImplTest {
     @Inject
     private UserTransaction userTransaction;
 
-    private NewsEntry entity;
-    private NewsEntryFullDto dto;
+    private Record entity;
+    private RecordFullDto dto;
     private UserLogin userLogin;
 
     @BeforeEach
     void setUp() {
-        entity = cloneNewsEntry1();
-        dto = cloneNewsEntryFullDto1();
+        entity = cloneRecord1();
+        dto = cloneRecordFullDto1();
         userLogin = cloneUserLogin1();
     }
 
@@ -117,7 +117,7 @@ class RecordFullCrudServiceImplTest {
     }
 
     @Test
-    void create(NewsEntryFullCrudService service) {
+    void create(RecordFullCrudService service) {
         Assertions.assertNotNull(service);
         when(mockDao.save(any())).thenReturn(entity);
         when(mockUserLoginDao.findById(any())).thenReturn(Optional.of(userLogin));
@@ -126,22 +126,22 @@ class RecordFullCrudServiceImplTest {
     }
 
     @Test
-    void readById(NewsEntryFullCrudService service) {
+    void readById(RecordFullCrudService service) {
         Assertions.assertNotNull(service);
         when(mockDao.findById(any())).thenReturn(Optional.of(entity));
         Assertions.assertEquals(dto, service.readById(entity.getId()));
     }
 
     @Test
-    void readRange(NewsEntryFullCrudService service) {
+    void readRange(RecordFullCrudService service) {
         Assertions.assertNotNull(service);
         when(mockDao.findById(any())).thenReturn(Optional.of(entity));
-        List<NewsEntryFullDto> testList = service.readRange(0, Integer.MAX_VALUE);
+        List<RecordFullDto> testList = service.readRange(0, Integer.MAX_VALUE);
         Assertions.assertTrue(testList.isEmpty());
     }
 
     @Test
-    void update(NewsEntryFullCrudService service) {
+    void update(RecordFullCrudService service) {
         Assertions.assertNotNull(service);
         when(mockDao.save(any())).thenReturn(entity);
         when(mockDao.findById(any())).thenReturn(Optional.of(entity));
@@ -151,7 +151,7 @@ class RecordFullCrudServiceImplTest {
     }
 
     @Test
-    void delete(NewsEntryFullCrudService service) {
+    void delete(RecordFullCrudService service) {
         Assertions.assertNotNull(service);
         service.delete(dto.getId());
     }
