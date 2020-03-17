f65d6f561eb8f96f2087b214f5c61924edf1a44a
Victor N. Skurikhin <vskurikhin@gmail.com>
2020-03-17 23:39:35 +0300

    Fix build issue.

--- src/main/db/db/Change-Sets/changelogs-2020-03-17-db-290.db_article.postgresql.sql
+++ src/main/db/db/Change-Sets/changelogs-2020-03-17-db-290.db_article.postgresql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE db.db_article (
     id          UUID PRIMARY KEY  NOT NULL DEFAULT pg_catalog.uuid_generate_v4(),
-                CONSTRAINT FK_db_news_links_need_record_4088
+                CONSTRAINT FK_db_article_need_record_4021
                 FOREIGN KEY (id)
                 REFERENCES db.db_record (id)
                 ON DELETE CASCADE ON UPDATE CASCADE,
