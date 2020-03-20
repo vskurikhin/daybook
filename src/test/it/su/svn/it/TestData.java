7d7dd0bb26078147bef0b9e6841b2e9245145f22
Victor Skurikhin <vs@rune.ru>
2020-03-20 23:09:20 +0300

    Added Article integration test.

--- src/test/it/su/svn/it/TestData.java
+++ src/test/it/su/svn/it/TestData.java
@@ -8,7 +8,6 @@
 
 package su.svn.it;
 
-import su.svn.shared.Constants;
 import su.svn.showcase.domain.*;
 import su.svn.showcase.dto.RecordDto;
 import su.svn.showcase.dto.TagBaseDto;
@@ -21,15 +20,19 @@ import java.util.*;
 public class TestData {
     public static final LocalDateTime NOW = LocalDateTime.now();
 
-    public static final String ID0 = "00000000" + "00000000";
-    public static final String ID1 = "00000000" + "00000001";
-    public static final String ID2 = "00000000" + "00000002";
-    public static final String ID3 = "00000000" + "00000003";
+    public static final String SID0 = "00000000" + "00000000";
+    public static final String SID1 = "00000000" + "00000001";
+    public static final String SID2 = "00000000" + "00000002";
+    public static final String SID3 = "00000000" + "00000003";
 
     public static final UUID UUID0 = new java.util.UUID(0, 0);
     public static final UUID UUID1 = UUID.fromString("00000000-0000-0000-0000-000000000001");
     public static final UUID UUID2 = UUID.fromString("00000000-0000-0000-0000-000000000002");
     public static final UUID UUID3 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+    public static final UUID UUID4 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+    public static final UUID UUID5 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+    public static final UUID UUID6 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+    public static final UUID UUID7 = UUID.fromString("00000000-0000-0000-0000-000000000003");
 
     public static final Set<Tag> EMPTY_TAGS = Collections.emptySet();
     public static final Set<Record> EMPTY_RECORDS = Collections.emptySet();
@@ -41,38 +44,10 @@ public class TestData {
     public static final List<UserRole> EMPTY_USER_ROLES = Collections.emptyList();
     public static final List<NewsEntry> EMPTY_NEWS_ENTRIES = Collections.emptyList();
 
-    public static final UUID ROLE_UUID0 = UUID0;
-    public static final UUID ROLE_UUID1 = UUID1;
-    public static final UUID ROLE_UUID2 = UUID2;
-    public static final UUID ROLE_UUID3 = UUID3;
-
-    public static final UUID USER_ROLE_UUID0 = UUID0;
-    public static final UUID USER_ROLE_UUID1 = UUID1;
-    public static final UUID USER_ROLE_UUID2 = UUID2;
-    public static final UUID USER_ROLE_UUID3 = UUID3;
-
-    public static final UUID USER_LOGIN_UUID0 = UUID0;
-    public static final UUID USER_LOGIN_UUID1 = UUID1;
-    public static final UUID USER_LOGIN_UUID2 = UUID2;
-    public static final UUID USER_LOGIN_UUID3 = UUID3;
-
-    public static final String TAG_ID0 = ID0;
-    public static final String TAG_ID1 = ID1;
-    public static final String TAG_ID2 = ID2;
-    public static final String TAG_ID3 = ID3;
-
-    public static final UUID NEWS_ENTRY_UUID0 = UUID0;
-    public static final UUID NEWS_ENTRY_UUID1 = UUID1;
-    public static final UUID NEWS_ENTRY_UUID2 = UUID2;
-    public static final UUID NEWS_ENTRY_UUID3 = UUID3;
-
-    public static final UUID NEWS_GROUP_UUID0 = UUID0;
-    public static final UUID NEWS_GROUP_UUID1 = UUID1;
-
-    public static final UUID RECORD_UUID0 = NEWS_ENTRY_UUID0;
-    public static final UUID RECORD_UUID1 = NEWS_ENTRY_UUID1;
-    public static final UUID RECORD_UUID2 = NEWS_ENTRY_UUID2;
-    public static final UUID RECORD_UUID3 = NEWS_ENTRY_UUID3;
+    public static final String TAG_ID0 = SID0;
+    public static final String TAG_ID1 = SID1;
+    public static final String TAG_ID2 = SID2;
+    public static final String TAG_ID3 = SID3;
 
     public static <T> List<T> newList(T o) {
         return new ArrayList<T>(){{add(o);}};
