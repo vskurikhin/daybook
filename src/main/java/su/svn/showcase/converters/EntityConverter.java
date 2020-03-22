5a214c896e4e04531530adce628ec244769d0583
Victor Skurikhin <vs@rune.ru>
2020-03-22 22:56:35 +0300

    Added interface EntityConverter.

---
+++ src/main/java/su/svn/showcase/converters/EntityConverter.java
@@ -0,0 +1,31 @@
+/*
+ * This file was last modified at 2020.03.22 22:56 by Victor N. Skurikhin.
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ * EntityConverter.java
+ * $Id$
+ */
+
+package su.svn.showcase.converters;
+
+import su.svn.showcase.domain.DBEntity;
+import su.svn.showcase.dto.Dto;
+
+public interface EntityConverter<K, E extends DBEntity<K>, D extends Dto<K>> {
+
+    /**
+     * TODO
+     *
+     * @param entity
+     * @return
+     */
+    D convert(E entity);
+
+    /**
+     * TODO
+     *
+     * @param dto
+     * @return
+     */
+    E convert(D dto);
+}
