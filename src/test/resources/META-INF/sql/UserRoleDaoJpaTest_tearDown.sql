bf92369bed847d12932a0613330e4b83daae5eb9
Victor N. Skurikhin <vs@lnhost.net>
2020-04-14 17:37:42 +0300

    Work with NewsEntryJdo.

--- src/test/resources/META-INF/sql/UserRoleDaoJpaTest_tearDown.sql
+++ src/test/resources/META-INF/sql/UserRoleDaoJpaTest_tearDown.sql
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2020.04.14 17:35 by Victor N. Skurikhin.
+ * This file was last modified at 2020.04.14 17:37 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * UserRoleDaoJpaTest_tearDown.sql
