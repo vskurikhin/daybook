14c24e7075949a317459e71f3b87f2ccf1673e62
Victor N. Skurikhin <vs@lnhost.net>
2020-04-22 23:04:54 +0300

    fix unit tests failed

--- src/test/resources/META-INF/sql/RecordCrudServiceImplTest_tearDown.sql
+++ src/test/resources/META-INF/sql/RecordCrudServiceImplTest_tearDown.sql
@@ -1,11 +1,3 @@
-/*
- * This file was last modified at 2020.04.22 23:01 by Victor N. Skurikhin.
- * This is free and unencumbered software released into the public domain.
- * For more information, please refer to <http://unlicense.org>
- * RecordCrudServiceImplTest_tearDown.sql
- * $Id$
- */
-
 DELETE FROM db.db_article;
 DELETE FROM db.db_news_entry;
 DELETE FROM db.db_news_links;
